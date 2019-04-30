1360dcbd5f3de1dca6dd8baff18c258d2e1e0004
Joan' Araujo <wisdombthegame@gmail.com>
2019-04-30 01:14:47 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,9 @@
 board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 
 def turn_count(board)
-  counter = 0
-
-  board.each do |turn|
-    if turn.downcase == "x" || turn.downcase == "o"
-      counter += 1
-      puts "#{counter}"
-    end
-  end
- counter
+board.count {|turn| turn == "X" || turn == "O"}
 end
 
-  def current_player(board)
-    turn_count(board) % 2 == 0 ? "X" : "O"
-  end
+def current_player(board)
+(turn_count(board) % 2 == 0 ? "X" : "O")
+end
