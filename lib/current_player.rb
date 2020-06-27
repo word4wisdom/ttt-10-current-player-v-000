940df3a406939af965d511a13f34d54963c2f101
Joan' Araujo <wisdombthegame@gmail.com>
2020-06-27 18:32:18 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,19 @@
 board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+#This method takes in an argument of the board array and returns the number
+#turns that have been played. A few things to think about:
+#Keep in mind the desired return value of the #turn_count method is the number of turns that have been played.
 
 def turn_count(board)
-board.count {|turn| turn == "X" || turn == "O"}
+  
+board.each do |turn_count|
+  counter = 0
+    if 
+turn_count == "X" || turn_count =="O"  counter += 1
+  end
 end
 
-def current_player(board)
-(turn_count(board).even? == true ? "X" : "O")
-end
+
+#1. Use iteration to loop over an array.
+#2. Control the return value of a method that uses iteration in the method body.
+#3. Describe and utilize the `%`, or modulo, operator.
+#4. Call a method from inside of another method.
