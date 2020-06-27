4de1477f64af171b1b0684c8cb6200fe85a6f0a5
Joan' Araujo <wisdombthegame@gmail.com>
2020-06-27 19:50:46 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,4 @@
 board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
-#This method takes in an argument of the board array and returns the number
-#turns that have been played. A few things to think about:
-#Keep in mind the desired return value of the #turn_count method is the number of turns that have been played.
 
 def turn_count(board)
   board.count{|turn| turn == "X" || turn == "O"}
